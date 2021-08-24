1b0e971326e9dd5bd84abf1b8d563447e285a40b
yanagswk <yanagimassu@gmail.com>
2021-08-24 18:44:08 +0900

    bootstrapの読み込みが重複しているのを修正, axiosの読み込みを記述

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,6 @@
 
-// require('./bootstrap');
 
-import './bootstrap';
+// import './bootstrap';
 import Vue from 'vue';
 import ArticleLike from './components/ArticleLike.vue';
 import ArticleTagsInput from './components/ArticleTagsInput.vue';
@@ -9,6 +8,9 @@ import FollowButton from './components/FollowButton.vue';
 
 
 window.Vue = require('vue').default;
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
